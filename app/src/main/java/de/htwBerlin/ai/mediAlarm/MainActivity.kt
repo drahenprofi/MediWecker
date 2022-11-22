f612c5ca196b20b638d089edcf1b79271cccdc74
RyanTT <10803066+RyanTT@users.noreply.github.com>
2022-11-23 00:25:33 +0100

    UI initial setup

--- app/src/main/java/de/htwBerlin/ai/mediAlarm/MainActivity.kt
+++ app/src/main/java/de/htwBerlin/ai/mediAlarm/MainActivity.kt
@@ -1,6 +1,8 @@
 package de.htwBerlin.ai.mediAlarm
 
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
@@ -9,15 +11,20 @@ import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import android.view.Menu
 import android.view.MenuItem
+import android.webkit.*
+import androidx.annotation.RequiresApi
+import androidx.webkit.WebViewAssetLoader
+import androidx.webkit.WebViewClientCompat
 import de.htwBerlin.ai.mediAlarm.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var appBarConfiguration: AppBarConfiguration
-    private lateinit var binding: ActivityMainBinding
+    //private lateinit var appBarConfiguration: AppBarConfiguration
+    //private lateinit var binding: ActivityMainBinding
+    private lateinit var webView: WebView;
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+        /*super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
@@ -31,10 +38,78 @@ class MainActivity : AppCompatActivity() {
         binding.fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                     .setAction("Action", null).show()
+        }*/
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Temporary webview setup
+        webView = findViewById(R.id.webView);
+
+        webView.settings.javaScriptEnabled = true;
+        webView.settings.javaScriptCanOpenWindowsAutomatically = true;
+        webView.settings.builtInZoomControls = true;
+        webView.settings.domStorageEnabled = true;
+
+        val assetLoader = WebViewAssetLoader.Builder()
+            .addPathHandler("/assets/", WebViewAssetLoader.AssetsPathHandler(this))
+            .addPathHandler("/res/", WebViewAssetLoader.ResourcesPathHandler(this))
+            .build()
+
+        /*webView.webViewClient = Callback();*/
+
+        webView.webViewClient = LocalContentWebViewClient(assetLoader)
+        webView.webChromeClient = LocalChromeClient();
+        webView.loadUrl("https://appassets.androidplatform.net/assets/wwwroot/index_mobile.html");
+    }
+
+    private class LocalChromeClient : WebChromeClient() {
+        override fun onConsoleMessage(consoleMessage: ConsoleMessage?): Boolean {
+            if (consoleMessage != null) {
+                Log.d("WebView", consoleMessage.message())
+            };
+
+            return true;
         }
     }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+    private class LocalContentWebViewClient(private val assetLoader: WebViewAssetLoader) : WebViewClientCompat() {
+        @RequiresApi(21)
+        override fun shouldInterceptRequest(
+            view: WebView,
+            request: WebResourceRequest
+        ): WebResourceResponse? {
+            Log.d("DEBUG", request.url.toString());
+
+            return assetLoader.shouldInterceptRequest(request.url)
+        }
+
+        // to support API < 21
+        override fun shouldInterceptRequest(
+            view: WebView,
+            url: String
+        ): WebResourceResponse? {
+            return assetLoader.shouldInterceptRequest(Uri.parse(url))
+        }
+
+        /*override fun shouldOverrideUrlLoading(
+            view: WebView?,
+            request: WebResourceRequest?
+        ): Boolean {
+            return false;
+        }*/
+    }
+
+
+    override fun onBackPressed() {
+        if (webView != null && webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    /*override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
         menuInflater.inflate(R.menu.menu_main, menu)
         return true
@@ -54,5 +129,5 @@ class MainActivity : AppCompatActivity() {
         val navController = findNavController(R.id.nav_host_fragment_content_main)
         return navController.navigateUp(appBarConfiguration)
                 || super.onSupportNavigateUp()
-    }
+    }*/
 }
