68d6cc9770fb2e6ee999b6cbcaa5427bd42efd14
drahenprofi <arndt.philipp@gmx.de>
2022-12-11 14:37:59 +0100

    Made TimepointType serializable

--- app/src/main/java/de/htwBerlin/ai/mediAlarm/data/rhythm/TimepointType.kt
+++ app/src/main/java/de/htwBerlin/ai/mediAlarm/data/rhythm/TimepointType.kt
@@ -1,10 +1,17 @@
 package de.htwBerlin.ai.mediAlarm.data.rhythm
 
+import com.google.gson.annotations.SerializedName
+
 enum class TimepointType
 {
+    @SerializedName("0")
     AbsoluteTime,
+    @SerializedName("1")
     Morning,
+    @SerializedName("2")
     Midday,
+    @SerializedName("3")
     Evening,
+    @SerializedName("4")
     Night
 }
